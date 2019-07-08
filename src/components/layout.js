3ec50af6bd7ab347a1d0653cec9968e1a49c81e7
Laura Jimenez <laurajimenez@laura-macbook-pro.clearc2.com>
2019-07-08 13:42:56 -0500

    layout margin changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,7 +12,7 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
-          marginTop: '5em',
+          marginTop: '7em',
           maxWidth: 960,
           padding: `0px 1.0875rem 1.45rem`,
           paddingTop: 0,
